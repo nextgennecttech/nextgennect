0435115eb843e811b80f689f925d8d7ed14923ec
nextgennecttech <nextgennect.tech@gmail.com>
2026-08-30 14:45:27 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { MentorsSection } from './components/MentorsSection';
 import { ThemeProvider } from './context/ThemeContext';
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
